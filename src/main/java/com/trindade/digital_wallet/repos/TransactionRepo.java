338051bc0769addb2a74a364f6be61c58b1b77d9
trindadeeesx <trindadesx@proton.me>
2025-11-17 15:23:59 -0300

    feat: add Javadoc comment for TransactionRepo interface

--- src/main/java/com/trindade/digital_wallet/repos/TransactionRepo.java
+++ src/main/java/com/trindade/digital_wallet/repos/TransactionRepo.java
@@ -3,5 +3,7 @@ package com.trindade.digital_wallet.repos;
 import com.trindade.digital_wallet.domain.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
+/**
+ * Repositório para gerenciar transações financeiras entre usuários
+ */
 public interface TransactionRepo extends JpaRepository<Transaction, Long> {}
