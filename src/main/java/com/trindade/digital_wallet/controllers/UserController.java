482cc97190dc361b3d79aac019c4c88e4264b117
trindadeeesx <trindadesx@proton.me>
2025-11-17 16:28:21 -0300

    fix: add decorator for spring to recognize params

--- src/main/java/com/trindade/digital_wallet/controllers/UserController.java
+++ src/main/java/com/trindade/digital_wallet/controllers/UserController.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,7 +23,7 @@ public class UserController {
 
 	// Cria um novo usuário
 	@PostMapping
-	public ResponseEntity<User> createUser(UserDTO user) {
+	public ResponseEntity<User> createUser(@RequestBody UserDTO user) {
 		User newU = this.service.createUser(user);
 
 		return new ResponseEntity<>(newU, HttpStatus.CREATED);
