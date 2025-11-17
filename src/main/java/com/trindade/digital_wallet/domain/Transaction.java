f4b29e5413f50f0d82d307691962160c152c5315
trindadeeesx <trindadesx@proton.me>
2025-11-17 15:17:02 -0300

    feat: add Transaction entity with attributes and JPA annotations

---
+++ src/main/java/com/trindade/digital_wallet/domain/Transaction.java
@@ -0,0 +1,32 @@
+package com.trindade.digital_wallet.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name="transactions")
+@Table(name="transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of="id")
+public class Transaction {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name="sender_id")
+	private User sender; // User ID of the sender
+
+	@ManyToOne
+	@JoinColumn(name="receiver_id")
+	private Long receiverId; // User ID of the receiver
+
+	private BigDecimal amount; // Transaction amount
+
+	private LocalDateTime timestamp; // Timestamp of the transaction
+}
