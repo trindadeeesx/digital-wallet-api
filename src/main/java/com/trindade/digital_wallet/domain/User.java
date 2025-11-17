243c18acc45147400cf4fa16854e7741b4e250a7
trindadeeesx <trindadesx@proton.me>
2025-11-17 15:16:56 -0300

    feat: add User entity with attributes and JPA annotations

---
+++ src/main/java/com/trindade/digital_wallet/domain/User.java
@@ -0,0 +1,35 @@
+package com.trindade.digital_wallet.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity(name="users")
+@Table(name="users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of="id")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String firstName;
+	private String lastName;
+
+	@Column(unique=true)
+	private String email;
+
+	@Column(unique=true)
+	private String document; // CPF/CNPJ
+
+	private String password; // won't be used
+
+	private BigDecimal balance; // user's wallet balance
+
+	@Enumerated(EnumType.STRING)
+	private UserRole role;
+}
